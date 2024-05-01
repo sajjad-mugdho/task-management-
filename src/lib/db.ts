d91df7d545857f67164c9bec178c6dee225d8b04
sajjad-mugdho <sajjadmugdho@yahoo.com>
2024-05-02 04:52:08 +0600

    [Fixed] auth route

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-export const db = globalThis.prisma || new PrismaClient();
+export const db =  new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
